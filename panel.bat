d6aabec60d80009040d1a93ddc39f184024dfb70
Pawel Gizinski <pawel.gizinski@me.pl>
2026-02-26 12:35:21 +0100

    change files locations

--- panel.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Uruchamianie panelu...
-python run_panel.py
--- test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Uruchamianie testu (Suite #1, Environment #1)...
-python main.py --suite 1 --environment 1
-pause
